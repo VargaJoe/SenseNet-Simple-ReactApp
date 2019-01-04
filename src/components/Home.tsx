f274fa72d03be9e52c8563c4191597a32f9b9994
vargajoe <varga.yosh@gmail.com>
2019-01-04 20:03:40 +0100

    make news column dynamic

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,39 +4,75 @@ import { Actions } from '@sensenet/redux';
 import {
 	withRouter
 } from 'react-router-dom';
-import LatestManganime from './LatestManganime';
-import LatestOther from './LatestOther';
-import LatestNews from './LatestNews';
-import Intro from './Intro';
 
+import NewsColumn from './NewsColumn';
+import Intro from './Intro';
+import { IODataParams } from '@sensenet/client-core';
+import { GenericContent } from '@sensenet/default-content-types';
+
+// const DATA = require('../config.json');
 // import Moment from 'react-moment';
 class Home extends React.Component<any, any> {
 	constructor(props: any) {
 		super(props);
 		this.state = {
 			isDataFetched: false,
-			articles: {}
+			columns: {},
+			ids: {}		
 		};
 	}
 
+	public componentDidMount() {
+        const colPath = '/'; // process.env.REACT_APP_COL_PATH || DATA.menuPath;
+        let colType = 'SmartFolder'; // process.env.REACT_APP_COL_TYPE || DATA.menuType;
+
+        let columns = this.props.getHomeContent(colPath, {
+			select: ['Name', 'Id', 'Path', 'Index', 'DisplayName'],
+			query: 'Type:' + colType + ' AND Hidden:0 .AUTOFILTERS:OFF',
+			orderby: ['Index', 'DisplayName']
+		} as IODataParams<GenericContent>);
+
+        columns.then((result: any) => {
+            console.log(result.value.entities.entities);
+            this.setState({
+                isDataFetched: true,
+				columns: result.value.entities.entities,
+				ids: result.value.result
+            });
+        });
+
+        columns.catch((err: any) => {
+            console.log(err);
+        });
+    }
+
 	public render() {
+		if (!this.state.isDataFetched) {
+            return null;
+        }
+		console.log(status);
+		
+		const columns = this.state.columns;
+		const colIds = this.state.ids;
+
+        // const menu = Object.keys(menuItems).map((key: any) =>
+        const colDOM = colIds
+			.map((key: number) =>
+            (      
+				<div>
+				{/* {columns[key].Name} */}
+				{/* <NewsColumn key={key} name={columns[key].DisplayName} pathTo={'/' + columns[key].Name} /> */}
+				<NewsColumn name={columns[key].DisplayName} pathTo={'/' + columns[key].Name}  />
+				</div>
+            )
+        );
+
 		return (
 			<div>
 			<Intro />
 			<div className="w3-row-padding">
 				<div className="w3-container w3-padding-large">
-					<div className="w3-third">
-					<h3>LEGFRISSEBB MANGA/ANIME</h3>					
-						<LatestManganime />
-					</div>
-					<div className="w3-third">
-						<h3>HÍREK</h3>
-						<LatestNews />
-					</div>
-					<div className="w3-third">
-						<h3>LEGFRISSEBB MIEGYMÁS</h3>
-						<LatestOther />
-					</div>		
+					{colDOM}
 				</div>
 			</div>
 		</div>
---
+++ src/components/NewsColumn.tsx
@@ -0,0 +1,138 @@
+import * as React from 'react';
+// import { PathHelper } from '@sensenet/client-utils';
+import { connect } from 'react-redux';
+import { Actions } from '@sensenet/redux';
+import Moment from 'react-moment';
+
+import { Folder } from '@sensenet/default-content-types';
+import { IODataParams } from '@sensenet/client-core';
+import { Link } from 'react-router-dom';
+// const DATA = require('../config.json');
+
+class LeisureArticle extends Folder {
+	PublishDate: Date;
+}
+
+interface Props {
+	pathTo:  string;
+	name: string;
+	getColumnContent: any;
+	repositoryUrl: string;
+}
+
+class NewsColumn extends React.Component<Props, any> {
+	
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			isDataFetched: false,
+			colItems: {},
+			ids: {}
+		};
+	}
+
+	componentWillReceiveProps(nextProps: any) {	
+		alert(nextProps.pathTo + '\r\n' + this.props.pathTo);	
+		if (nextProps.pathTo !== this.props.pathTo) {
+			this._initializeComponent();
+		}						
+	}	
+
+	componentDidMount() {
+		// no
+		// alert(this.props.name + '\r\n' + this.props.pathTo);		
+		this._initializeComponent();
+	}
+
+	_initializeComponent() {
+		let path = this.props.pathTo;
+		// get the current user info
+		let itemGet = this.props.getColumnContent(path, {
+			select: ['Type', 'CreationDate', 'CreatedBy', 'Description', 'DisplayName', 'Id', 'Lead', 'OriginalAuthor', 'Author', 'PublishDate', 'Index', 'Actions'],
+			expand: ['CreatedBy', 'Actions/SOxSOImg'],
+			orderby: [['PublishDate', 'desc'], ['Index', 'desc'], 'DisplayName'],
+		} as IODataParams<LeisureArticle>);
+
+		itemGet.then((result: any) => {
+			console.log(result.value.entities.entities);
+			this.setState({
+				isDataFetched: true,
+				colItems: result.value.entities.entities,
+				ids: result.value.result
+			});
+		});
+
+		itemGet.catch((err: any) => {
+			console.log(err);
+		});
+	}
+
+	public render() {
+		if (!this.state.isDataFetched) {
+            return null;
+        }
+        console.log(status);
+        
+        const colItems = this.state.colItems;
+        const colIds = this.state.ids;
+
+		const column = colIds
+			.map((key: number) =>
+			(   
+				(colItems[key].Type === 'LeisureArticle') ? showNews(this.props.repositoryUrl, key, colItems[key]) : showReview(this.props.repositoryUrl, key, colItems[key])
+				
+			)
+		);
+
+		return (
+			<div className="w3-third">
+				<h3>{this.props.name}</h3>
+				<ul className="w3-ul w3-hoverable">
+					{column}
+				</ul>
+			</div>
+		);
+	}
+}
+
+function showReview(repoUrl: any, key: any, article: any) {
+	return (
+		<li key={key} className="w3-padding-16">
+			<Link className="no-score" to={'/News/' + article.Name}>		
+					<img src={repoUrl + article.Actions.find(function (obj: any) { return obj.Name === 'SOxSOImg'; }).Url} className="w3-left w3-margin-right news-img" />
+					<span className="w3-large">{article.DisplayName}</span>
+					<br/>
+					<span className="hidden">{article.Description}</span>
+					<span className="small hidden">{article.Author}</span>
+					<span><Moment date={article.PublishDate} format="YYYY.MM.DD." /></span>
+			</Link>
+		</li>
+	);
+}
+
+function showNews(repoUrl: any, key: any, article: any) {
+	return (
+		<li key={key} className="w3-padding-16">
+			<Link className="no-score"  to={'/News/' + article.Name}>
+				<Moment date={article.PublishDate} format="YYYY.MM.DD. " />:
+				{article.DisplayName}<br />
+				<div className="small">{article.Lead}</div>
+			</Link>
+		</li>
+	);
+}
+
+const mapStateToProps = (state: any, match: any) => {
+	return {
+		userName: state.sensenet.session.user.userName,
+		repositoryUrl: state.sensenet.session.repository.repositoryUrl,
+	};
+};
+
+// export default NewsColumn;
+export default connect(
+	mapStateToProps,
+	(dispatch) => ({
+		getColumnContent: (path: string, options: any) => dispatch(Actions.requestContent(path, options)),
+	})
+)(NewsColumn as any);
