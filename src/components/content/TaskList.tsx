a59ffdbd76caae0ee8f10b71e5b8bd9c300a220d
vargajoe <varga.yosh@gmail.com>
2019-08-24 20:12:09 +0200

    add tasklist

---
+++ src/components/content/TaskList.tsx
@@ -0,0 +1,16 @@
+import * as React				from 'react';
+
+class TaskList extends React.Component<any, any> {
+    public render() {
+
+		return (
+			<div>
+				<h1>TaskList</h1>
+				
+			</div>
+		);
+	}
+}
+
+export default TaskList;
+ 
